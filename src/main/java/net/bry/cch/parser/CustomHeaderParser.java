1dd5ed2d4efe0d33a93dd323da8049efafb7d0c0
TheBryBry154 <thebrybry154@gmail.com>
2026-07-04 16:26:38 -0400

    its the little things in life

--- src/main/java/net/bry/cch/parser/CustomHeaderParser.java
+++ src/main/java/net/bry/cch/parser/CustomHeaderParser.java
@@ -18,10 +18,24 @@ public class CustomHeaderParser {
 
     public static String defaultFileContents() {
        return  "# -------------------------------------------------------\n" +
-               "# MMMSoDNONAFOINOINOEIOIONFAONDOifnAOFNPDPNIFOIJINOINOI  \n" +
+               "# Thanks for using Custom Crash Headers!                 \n" +
+               "# this mod was really made in mind for people who might want to make a modpack, (for smps/servers or the sort)\n" +
+               "# and want to add some info if a user's game crashes. (contact info/ where to ask for help)\n" +
+               "# \n" +
+               "# any bugs, questions, etc. should probably go to the github - https://github.com/TheBryBry154/Custom-Crash-Headers\n" +
+               "# or message me on discord at thebrybrybrybrybrybrybrybry154 ig, idk i dont really have a discord server tbh\n" +
+               "# anyways have fun, -Bry\n" +
+               "# \n" +
+               "# HOW TO USE: \n" +
+               "# - anything with a pound (hashtag?) symbol in front of it is counted as a comment and will not be displayed in the crash message\n" +
+               "# the *>Random setting below can be used to make a set of headers that will be picked at random.\n" +
+               "# In this mode every line is counted as a separate entry\n" +
                "# -------------------------------------------------------\n" +
-               "*>Random = false                                         \n" +
-               "oh dear";
+               "*>Random = false                                         \n\n" +
+               "ruh roh\n" +
+               " ";
+
+
     }
 
     public static List<String> parseUsableFromFile() throws IOException {
